9cf438327fdd211b6a808b1e2ee860f7fd77bb06
Best Cat <kotova-irina11@rambler.ru>
2022-09-19 20:22:31 +0300

    задача 43 точка перечесения 2х прямых

--- ex_2/Program.cs
+++ ex_2/Program.cs
@@ -2,18 +2,13 @@
 //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 Console.WriteLine("Введите первую точку первой прямой(b1): ");
-int b1 = int.Parse(Console.ReadLine());
+double b1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите вторую точку первой прямой(b2): ");
-int b1 = int.Parse(Console.ReadLine());
+double b2 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите первую точку второй прямой(k1): ");
-int k1 = int.Parse(Console.ReadLine());
+double k1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите вторую точку второй прямой(k2): ");
-int k2 = int.Parse(Console.ReadLine());
-int Method(int x) 
-{
-x = b2-b1 / k1 -k2
-}
-return x;
-
-int x = Sum(x);
-Console.WriteLine($"x = {x}");
+double k2 = int.Parse(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"точка пересечения двух прямых = ({x} , {y})");
