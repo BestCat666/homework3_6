c18596de53658734187f09781dcbfd4bd76c541d
Best Cat <kotova-irina11@rambler.ru>
2022-09-19 18:30:36 +0300

    задача 41 кол-во положительных чисел вариант2

---
+++ ex_1var2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите числа: ");
+int [] num = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int Method1(int[] col)
+{
+    int count = 0;
+    for (int i = 0; i < col.Length; i++)
+    {
+        if (num[i] > 0);
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int rez = Method1(num);
+Console.WriteLine($"количество положительных чисел = {rez}");
