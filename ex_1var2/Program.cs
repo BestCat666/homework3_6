1b23e5b7f6fad7e16431728646c11b13eea05ad6
Best Cat <kotova-irina11@rambler.ru>
2022-09-19 20:19:15 +0300

    задача 41 кол-во положительных чисел вариант2

--- ex_1var2/Program.cs
+++ ex_1var2/Program.cs
@@ -1,19 +1,38 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
+
 Console.WriteLine("Введите числа: ");
-int [] num = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int Method1(int[] col)
+int[] num = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+//int rez = 0;
+//foreach (int el in num)
+//{
+//  if (el > 0)
+//{
+//  rez = rez + 1;
+//}
+//}
+//Console.WriteLine($"количество положительных числе = {rez}");
+void PrintArray(int[] collection)
 {
-    int count = 0;
-    for (int i = 0; i < col.Length; i++)
-    {
-        if (num[i] > 0);
-        {
-            count++;
-        }
-    }
+   for (int i = 0; i < collection.Length; i++)
+{
+   Console.Write($"{(i == 0 ? collection[i] : " " + collection[i])}");
+}
+Console.WriteLine();
+}
+PrintArray(num);
+
+int PositiveNum(int[] col)
+{ 
+   int count = 0;
+   for (int i = 0; i < col.Length; i++)
+   {
+    if (col[i] > 0)
+   {
+       count++;
+   }
+   }
     return count;
 }
-int rez = Method1(num);
-Console.WriteLine($"количество положительных чисел = {rez}");
+Console.WriteLine($"количество положительных чисел = {PositiveNum(num)}");
